ce7d8fbbb417615495cac3b0fd894e0c88deacf6
Dave Muzeni <kmuzeni@gmail.com>
2017-02-18 16:16:52 -0500

    Not much to it but sending it

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+
+
+var randLetters=["a","b","c","d","e","f","g"]
+
+for(var i=0; i<randLetters.length, i++);
+
+
+var wins= 0;
+var loses= 0;
+var guessesLeft= 10;
+var yourGuessesSoFar= 0;
+
+if(wins){
+	win++
+}
+
+if(loses){
+	loses++
+}
+
+if(userGuess === randLetter){
+	wins++
+}
+
+if(userGuess !== randLetter){
+	loses--
+	guessesLeft--
+}
+
+
+ var randomLetter = Math.floor(Math.random() +"randomLetter.length" * 9) + 1; 
+
+
+
+ 
+
